d200c57175251035a85afb1973d2f323dcada0d1
Shazvi <shazvi@outlook.com>
2020-02-19 19:19:58 +0530

    fixed issue in checking whether current page is index page

--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,12 @@
+<?php 
+$isIndexPage = substr_compare($_SERVER["PHP_SELF"], '/index.php', -strlen('/index.php')) === 0;
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <?php if (isset($title)): ?>
-        <title>My Chrome: <?= htmlspecialchars($title) ?></title>
+        <title>My Chrome: <?php echo htmlspecialchars($title) ?></title>
     <?php else: ?>
         <title>My Chrome</title>
     <?php endif ?>
@@ -14,16 +18,16 @@
     <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <!-- <link href="css/nav.css" rel="stylesheet"> -->
-    <?php if($_SERVER["PHP_SELF"]=="/index.php"): ?>
+    <?php if($isIndexPage): ?>
         <link rel="stylesheet" href="css/easyTree.css">
         <script type="text/javascript" src="js/easyTree.js"></script>
         <script type="text/javascript" src="js/jquery.highlight-upd.js"></script>
     <?php endif ?>
 </head>
-<body <?php if($_SERVER["PHP_SELF"]=="/index.php")//echo'style="padding-top: 70px;"';?>>
+<body <?php if($isIndexPage)//echo'style="padding-top: 70px;"';?>>
 <div class="container">
-    <?php if($_SERVER["PHP_SELF"]!="/index.php"): ?>
+    <?php if(!$isIndexPage): ?>
         <div class="page-header">
-            <h1><?=isset($title)?$title:"";?> <small> Access your chrome bookmarks anywhere</small></h1>
+            <h1><?php echo isset($title) ? $title : ""; ?> <small> Access your chrome bookmarks anywhere</small></h1>
         </div>
     <?php endif ?>
